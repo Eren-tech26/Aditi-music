51e94c5f036bdc9ab0887f868fc69aeb68185d35
Eren-tech26 <Rohanishwarkatti06@gmail.com>
2026-07-18 16:53:28 +0530

    fix handler registration

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import asyncio
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
-import commands
 from aiohttp import web
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+SESSION_STRING = os.environ.get("SESSION_STRING")
 
 app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 user = Client("user", api_id=API_ID, api_hash=API_HASH, session_string=SESSION_STRING)
 call_py = PyTgCalls(user)
 
+import commands
+commands.set_call(call_py)
+commands.register(app)
+
 async def handle(request):
     return web.Response(text="Aditi Music Bot is running!")
 
 async def main():
-    commands.set_call(call_py)
-    commands.register(app)
     await user.start()
     await call_py.start()
     await app.start()
     print("✅ Aditi Music Bot is running!")
 
-    # fake web server so Render doesn't kill us
     server = web.Application()
     server.router.add_get("/", handle)
     runner = web.AppRunner(server)
