4b2cfee991f304b6ed1e2b675613b36723d78ced
Eren-tech26 <Rohanishwarkatti06@gmail.com>
2026-07-18 16:38:01 +0530

    add web server for render

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import asyncio
-import sys
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 from config import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
 import commands
+from aiohttp import web
 
 app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 user = Client("user", api_id=API_ID, api_hash=API_HASH, session_string=SESSION_STRING)
 call_py = PyTgCalls(user)
+
+async def handle(request):
+    return web.Response(text="Aditi Music Bot is running!")
 
 async def main():
     commands.set_call(call_py)
@@ -16,6 +19,15 @@ async def main():
     await call_py.start()
     await app.start()
     print("✅ Aditi Music Bot is running!")
+
+    # fake web server so Render doesn't kill us
+    server = web.Application()
+    server.router.add_get("/", handle)
+    runner = web.AppRunner(server)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
+    await site.start()
+
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
